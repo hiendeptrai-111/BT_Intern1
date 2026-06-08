4e8ec27c9b12e7cfa90f5ef992e492836c358859
Tran Van Hien <hien_2251220165@dau.edu.vn>
2026-06-08 10:13:06 +0700

    update

--- book_manage/settings.py
+++ book_manage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%-8%^*9_$ss^113@c3k15(pru_w&(c=6!50ty13+f9h6m7m)1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
